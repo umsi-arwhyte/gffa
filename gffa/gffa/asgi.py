bcba3c63e1a64d9de6a9fd3d4c483893fbcd056f
shaiviganatra <krishaivi15@gmail.com>
2022-06-10 12:18:34 -0400

    django and postgresql setup done, commands added to readme

--- gffa/gffa/asgi.py
+++ gffa/gffa/asgi.py
@@ -4,11 +4,7 @@ ASGI config for gffa project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-<<<<<<< HEAD
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-=======
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
->>>>>>> 9f3b8368c63a623cee5f5551eaa906c500ed1e5f
 """
 
 import os
